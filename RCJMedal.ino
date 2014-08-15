3185a0817966e23062aa03e59a39da3a178dc4db
Tom Manderson <TRManderson@gmail.com>
2014-08-15 10:32:24 +1000

    Commenting is generally a good idea

--- RCJMedal.ino
+++ RCJMedal.ino
@@ -3,6 +3,7 @@
 
 void setup(){
   pinMode(LED,OUTPUT);
+  
   //flash three times
   digitalWrite(LED,LOW);
   delay(500);
@@ -21,18 +22,25 @@ void setup(){
 
 #ifndef CAPACITIVE
 void loop(){
+  //slowly ramp up to full
   for(int i=0; i<255;i++;){
     analogWrite(LED,i);
     delay(10);
   }
+  delay(200); //keep at max
+  
+  //slowly ramp down
   for(int i=255;i>0;i--;){
     analogWrite(LED,i);
     delay(10);
   }
+  
+  //flash on for a second
   delay(200);
   digitalWrite(LED,HIGH);
   delay(1000);
   digitalWrite(LED,LOW);
+  delay(200);
 }
 #endif
 
