da761cb1fb842af2b71f47493a6a503bef47f8b0
Tom Manderson <TRManderson@gmail.com>
2014-09-22 18:53:02 +1000

    Actual production code for QLD medallions

--- RCJMedal.ino
+++ RCJMedal.ino
@@ -18,34 +18,31 @@ void setup(){
   digitalWrite(LED,HIGH);
   delay(500);
   digitalWrite(LED,LOW);
+  delay(500);
+  
+  //for 10 minutes
+  while(millis() < 600000){
+    //slowly ramp up to full
+    for(int i=0; i<255;i++){
+      analogWrite(LED,i);
+      delay(10);
+    }
+    delay(200); //keep at max
+    
+    //slowly ramp down
+    for(int i=255;i>0;i--){
+      analogWrite(LED,i);
+      delay(10);
+    }
+    
+    //flash on for a second
+    delay(200);
+    digitalWrite(LED,HIGH);
+    delay(1000);
+    digitalWrite(LED,LOW);
+    delay(200);
+  }
 }
 
-#ifndef CAPACITIVE
 void loop(){
-  //slowly ramp up to full
-  for(int i=0; i<255;i++;){
-    analogWrite(LED,i);
-    delay(10);
-  }
-  delay(200); //keep at max
-  
-  //slowly ramp down
-  for(int i=255;i>0;i--;){
-    analogWrite(LED,i);
-    delay(10);
-  }
-  
-  //flash on for a second
-  delay(200);
-  digitalWrite(LED,HIGH);
-  delay(1000);
-  digitalWrite(LED,LOW);
-  delay(200);
 }
-#endif
-
-#ifdef CAPACITIVE
-void loop(){
-  
-}
-#endif
