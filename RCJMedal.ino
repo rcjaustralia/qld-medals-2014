fca6b37118f71827f73a75351be099cc4a752212
Tom Manderson <TRManderson@gmail.com>
2014-09-22 21:43:17 +1000

    Capacative touch working!

--- RCJMedal.ino
+++ RCJMedal.ino
@@ -1,48 +1,54 @@
-#define LED 1
-//#define CAPACITIVE
+#include <avr/sleep.h>
+#include <avr/power.h>
 
-void setup(){
-  pinMode(LED,OUTPUT);
-  
-  //flash three times
-  digitalWrite(LED,LOW);
-  delay(500);
-  digitalWrite(LED,HIGH);
-  delay(500);
-  digitalWrite(LED,LOW);
-  delay(500);
-  digitalWrite(LED,HIGH);
-  delay(500);
-  digitalWrite(LED,LOW);
-  delay(500);
-  digitalWrite(LED,HIGH);
-  delay(500);
-  digitalWrite(LED,LOW);
-  delay(500);
-  
-  //for 10 minutes
-  while(millis() < 600000){
-    //slowly ramp up to full
-    for(int i=0; i<255;i++){
-      analogWrite(LED,i);
-      delay(10);
-    }
-    delay(200); //keep at max
-    
-    //slowly ramp down
-    for(int i=255;i>0;i--){
-      analogWrite(LED,i);
-      delay(10);
-    }
-    
-    //flash on for a second
-    delay(200);
-    digitalWrite(LED,HIGH);
-    delay(1000);
-    digitalWrite(LED,LOW);
-    delay(200);
+int capread(int pin){
+  //Drive the pin high
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,HIGH);
+  //Prepare to read
+  pinMode(pin,INPUT);
+  digitalWrite(pin,LOW);
+  int t=millis();
+  //Wait until pin is low or 0.8 seconds have elapsed
+  while(digitalRead(pin) && millis() -t < 800);
+  t=millis()-t;
+  //If it took more than half a second, return 0
+  if(t >=500){
+    return 0;
+  } else{
+    return 1;
   }
 }
 
+
+void setup(){
+  pinMode(1,OUTPUT);
+  digitalWrite(1,1);
+  delay(1000);
+}
+
+
+int zeroCount=0;
+int sleep=0;
 void loop(){
+  int input = capread(2);
+  if(input==1){
+    zeroCount=0;
+  }else{
+    zeroCount+=1;
+  }
+  digitalWrite(1, input);
+  if(zeroCount > 100){
+    while(!capread(2)){
+      if(millis() > 600000) sleep=1;
+      delay(100);
+    }
+    zeroCount = 0;
+  }
+  delay(100);
+  if(sleep){ //Power down
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    sleep_mode();
+  }
 }
